1c7955e7eef7e63a8d72144b94b488d9ed639342
Michael Specht <micha.specht@gmail.com>
2025-08-07 16:26:16 +0200

    yay

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-code-server --install-extension ritwickdey.liveserver ms-python.python ms-toolsai.jupyter
+code-server --install-extension ritwickdey.liveserver
+code-server --install-extension ms-python.python
+code-server --install-extension ms-toolsai.jupyter
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
