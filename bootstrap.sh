7a6d298a45f54afe09fa38ac9edb4d9052562227
Michael Specht <micha.specht@gmail.com>
2025-08-07 16:14:37 +0200

    yay

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+code-server --install-extension ritwickdey.liveserver
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-code-server --install-extension ritwickdey.liveserver
 
 echo "✅ Pakete installiert"
